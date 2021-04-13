8211760a66c972796ab1a178fb81113ae90876ef
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2021-04-13 15:42:07 +0100

    move to buster (#9)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.0] - 2021-04-13
+
+### Changed
+
+- Switch base image to official Python Buster image. ([#9](https://github.com/glitchcrab/action-hass-check-config/pull/9))
+
 ## [0.1.0] - 2020-12-30
 
 ### Added
@@ -18,6 +24,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Initial release.
 
-[Unreleased]: https://github.com//glitchcrab/action-hass-check-config/compare/v0.1.0...HEAD
+[Unreleased]: https://github.com//glitchcrab/action-hass-check-config/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/glitchcrab/action-hass-check-config/releases/tag/v0.2.0
 [0.1.0]: https://github.com/glitchcrab/action-hass-check-config/releases/tag/v0.1.0
 [0.0.1]: https://github.com/glitchcrab/action-hass-check-config/releases/tag/v0.0.1
--- runaction.sh
+++ runaction.sh
@@ -3,12 +3,21 @@
 declare statuscode
 statuscode=0
 
+VENVDIR="/hass-venv/bin"
+
 echo "::info:: Installing Home Assistant"
 
+if [ ! -d "${VENVDIR}" ]; then
+  echo "Python virtualenv doesn't exist at ${VENVDIR}; exiting"
+  exit 1
+fi
+
+source ${VENVDIR}/activate
+
 if [ ! -z "${INPUT_VERSION}" ]; then
-  python3 -m pip install --disable-pip-version-check git+git://github.com/home-assistant/home-assistant.git@${INPUT_VERSION} || statuscode=1
+  ${VENVDIR}/python -m pip install --disable-pip-version-check git+git://github.com/home-assistant/home-assistant.git@${INPUT_VERSION} || statuscode=1
 else
-  python3 -m pip install --disable-pip-version-check homeassistant || statuscode=1
+  ${VENVDIR}/python -m pip install --disable-pip-version-check homeassistant || statuscode=1
 fi
 
 if [ -d "${INPUT_CONFIG_PATH}/custom_components/" ]; then
@@ -19,13 +28,13 @@ if [ -d "${INPUT_CONFIG_PATH}/custom_components/" ]; then
 
       for requirement in $(jq -r '.[]' <<< "$(jq '.requirements' "$manifest")"); do
         echo "::info:: Installing requirement '$requirement'"
-        python3 -m pip install --disable-pip-version-check "$requirement" || statuscode=1
+        ${VENVDIR}/python -m pip install --disable-pip-version-check "$requirement" || statuscode=1
       done
     done
 fi
 
 echo "::info:: Running config check"
-hass --script check_config --config "${GITHUB_WORKSPACE}"/"${INPUT_CONFIG_PATH}" || statuscode=1
+${VENVDIR}/hass --script check_config --config "${GITHUB_WORKSPACE}"/"${INPUT_CONFIG_PATH}" || statuscode=1
 
 echo "::info:: Config check is complete"
 exit "$statuscode"
